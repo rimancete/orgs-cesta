a73c1713c6b6965794a2b4183b85c7545dbcc23a
Osvaldo Costa <osvaldo.costajr@gmail.com>
2023-01-07 10:05:16 -0300

    S4C08 - Para saber mais: Eject

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
   }
   
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{flex: 1}}>
       <StatusBar />
       <Cesta {...mock} />
     </SafeAreaView>
--- src/screens/Cesta/index.js
+++ src/screens/Cesta/index.js
@@ -1,16 +1,28 @@
-import { StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 import Body from './components/Body';
+import CustomText from '../../components/CustomText';
 import Header from './components/Header';
+import Item from './components/Item';
 
-export default function Cesta({ header, body}) {
+export default function Cesta({ header, body, items}) {
 
     return (
         <>
-            <Header {...header}/>
-            
-            <View style={styles.contentView}>
-                <Body {...body} />
-            </View>
+            <FlatList 
+                data={items.list}
+                renderItem={Item}
+                keyExtractor={({name}) => name}
+                ListHeaderComponent={() => {
+                    return <>
+                        <Header {...header}/>
+
+                        <View style={styles.contentView}>
+                            <Body {...body} />
+                            <CustomText style={styles.itemTitle}>{items.title}</CustomText>
+                        </View>
+                    </>
+                }}
+            />
         </>
     );
 }
@@ -19,5 +31,13 @@ const styles = StyleSheet.create({
     contentView: {
         paddingVertical: 8,
         paddingHorizontal: 16
+    },
+    itemTitle: {
+        color: "#464646",
+        fontWeight: 'bold',
+        marginTop: 32,
+        marginBottom: 8,
+        fontSize: 20,
+        lineHeight: 32
     },
 });
